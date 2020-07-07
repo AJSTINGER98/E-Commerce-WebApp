2c7f038a29dade89b080f072a4c1e690353802bb
AJSTINGER98 <amann.jha@gmail.com>
2020-07-07 16:51:28 +0530

    navbar design phase-1

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,33 +1,65 @@
 <template>
   <div id="#app">
-    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" :class= "{'bg-dark': hasScrolled,'bg-transparent' : !hasScrolled }">
-      <a class="navbar-brand" href="#">Taknikki Factorry</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" @click=" checkScroll ? hasScrolled = !hasScrolled : hasScrolled = hasScrolled">
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" :class= "{'background-dark': hasScrolled,'bg-transparent' : !hasScrolled }">
+      <a class="navbar-brand nav-header navbar-mobile" :class="{'nav-header-scroll': hasScrolled}"  href="#"><img src="./assets/nav-logo.png" alt="nav-logo"></a>
+      <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" @click="isToggled = true, handleScroll()">
         <span class="navbar-toggler-icon"></span>
       </button>      
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-0 w-100">
+      <!-- NAVBAR -->
+      <div class="collapse navbar-collapse container" :class="{'navbar-small' : hasScrolled,}" v-if="checkWidth">
+        <ul class="navbar-nav mr-0 w-100 nav-body" >
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="#" selected><span> HOME</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
+            <a class="nav-link" href="#"><span>ABOUT</span></a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown
+            <a class="nav-link dropdown-toggle " href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              DROPDOWN
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">ACTION</a>
+              <a class="dropdown-item" href="#">ANOTHER aCTION</a>
               <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
+              <a class="dropdown-item" href="#">SOMETHING ELSE</a>
             </div>
           </li>
-          <li class="nav-item ml-auto">
-            <a class="nav-link" href="#" @click="isModalVisible = true">Login / SignUp</a>
+          <div class="ml-auto d-flex d-inline-block">
+            <li class="nav-item">
+              <a class="nav-link" href="#" @click="isModalVisible = true"><i class="far fa-user fa-lg"></i></a>
+            </li>
+            <li class="">
+              <a class="nav-link" href="#" @click="isModalVisible = true"><i class="fas fa-shopping-cart fa-lg"></i></a>
+            </li>
+          </div>
+        </ul>
+      </div>
+      <!-- SIDEBAR -->
+      <div class="sidebar" :class="{'sidebar-active' : isToggled}"  v-else>
+          <a href="javascript:void(0)" class="closebtn" @click="isToggled = false,handleScroll()">&times;</a>
+          <ul class="navbar-nav w-100" >
+          <li class="nav-item">
+            <a class="nav-link" href="#" selected>HOME</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"> ABOUT</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle " href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              DROPDOWN
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item py-2 px-4" href="#">ACTION</a>
+              <a class="dropdown-item py-2 px-4" href="#">ANOTHER ACTION</a>
+              <div class="dropdown-divider m-0"></div>
+              <a class="dropdown-item py-2 px-4" href="#">SOMETHING ELSE HERE</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" @click="isModalVisible = true">LOGIN</a>
+          </li>
+      
         </ul>
       </div>
     </nav>
@@ -89,26 +121,27 @@ export default {
   data (){
     return {
       hasScrolled : false,
+      isToggled : false,
       updateScroll : window.scrollY,
       isModalVisible : false,
-      currentAuthPage : 'login'
+      currentAuthPage : 'login',
+      window : {
+        width : 0,
+        height : 0
+      }
 
     }
   },
   created () {
     window.addEventListener('scroll', this.handleScroll);
+    window.addEventListener('resize',this.windowSize);
+    this.windowSize();
   },
   
-  computed: {
-    checkScroll(){
-      return this.updateScroll < 100 ? true : false;
-    }
-  },
-
   methods: {
     handleScroll(){
       this.updateScroll = window.scrollY; 
-      if(window.scrollY > 100){
+      if(window.scrollY > 100 || this.isToggled){
         this.hasScrolled = true;
       } else{
         this.hasScrolled = false;
@@ -120,9 +153,18 @@ export default {
       setTimeout(() => {
         vm.currentAuthPage = 'login';
       },1000)
+    },
+    windowSize() {
+      this.window.width = window.innerWidth;
+      this.window.height = window.innerHeight;
     }
     
     
+  },
+  computed : {
+    checkWidth() {
+      return this.window.width > 990 ? true : false;
+    },
   },
 
   components: {
@@ -137,16 +179,126 @@ export default {
   .navbar{
     transition: background 0.5s;
   }
-  .navbar .navbar-brand {
-    margin-right: 20%;
-    
-  }
 
   .navbar ul li {
     padding-right : 50px;
   }
   .blur-effect {
     filter:blur(8px);
+  }
+
+
+  .nav-header {
+    position: absolute;
+    left: 47vw;
+    padding-top: 40px;
+    margin: 0;
+    background-attachment: fixed;
+    transform: scale(1.2);
+    transition: all 0.5s ease;
+  }
+  .nav-header-scroll{
+    padding-top: 0;
+    padding-top: 40px;
+    transform: scale(0.8);
+  }
+  .navbar{
+    font-size: 15px;
+  }
+  
+  .background-dark {
+    background-color: black;
+    color: white;
+    
+  }
+  .nav-body a{
+    color: white;
+
+  }
+
+
+  @media screen and (max-width: 990px){
+    .navbar-mobile {
+      
+      transform: scale(0.7);
+      display: flex;
+      left: 80%;
+    
+    }
+  }
+
+  .sidebar {
+    position: absolute;
+    left: 0;
+    top: 0;
+    background: black;
+    height: 100vh;
+    width: 0;
+    color: white;
+    transition: width 0.7s ease;
+    overflow-x: hidden;
+    z-index: 1;
+  }
+  .sidebar-active {
+
+    width: 250px;
+  }
+  .sidebar ul {
+    position: sticky;
+    margin-left: 5px;
+  }
+  .sidebar ul li {
+
+    list-style-type: none;
+    padding: 10px 5px;
+    width: 100%;
+  }
+
+  .dropdown-menu {
+    transition: all 0.3s ease;
+    border-radius: 0;
+    background: black;
+    color: white;
+    margin-right: 5px;
+    border: 1px solid white;
+    padding: 0;
+  }
+
+  .dropdown-menu .dropdown-item:hover{
+    background: white;
+    color: black;
+  }
+
+  .sidebar ul li a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  .sidebar .closebtn {
+    color: inherit;
+    position: relative;
+    margin-right: 10px;
+    float: right;
+    font-size: 40px;
+    text-decoration: none;
+    padding-top: 4px;
+    height: 10%;
+  }
+  .nav-link span::after{
+    content: '';
+    display: block;
+    width: 100%;
+    height: 1px;
+    background-color: white  ;
+    bottom: 0;
+    left: 0;
+    transform: scaleX(0);
+    transition: transform 0.3s ease-in-out;
+  }
+  
+
+  .nav-link:hover span:after {
+    transform: scaleX(1);
   }
 
 </style>
