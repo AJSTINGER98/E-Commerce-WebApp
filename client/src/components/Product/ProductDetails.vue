7a837d49b5af2ac660fccf5543ecc0783e341fbf
AJSTINGER98 <amann.jha@gmail.com>
2020-08-08 20:14:06 +0530

    merge branch

--- client/src/components/Product/ProductDetails.vue
+++ client/src/components/Product/ProductDetails.vue
@@ -106,57 +106,57 @@ export default {
       qtymax : 20,
       qtymin : 1,
       product : {
-        // image: [
-        //   {
-        //     src : 'https://cnet1.cbsistatic.com/img/aJE3TOIiSEqz_oFN80Y_1dQ0ucg=/1092x0/2019/08/16/99be5eac-2d91-4011-8c5e-89f328d88e67/geforce-rtx-2080-ti-web-tech-shot-630-u2x.png',
-        //     alt: 'image-1'
-        //   },
-        //   {
-        //     src : 'https://cnet1.cbsistatic.com/img/aJE3TOIiSEqz_oFN80Y_1dQ0ucg=/1092x0/2019/08/16/99be5eac-2d91-4011-8c5e-89f328d88e67/geforce-rtx-2080-ti-web-tech-shot-630-u2x.png',
-        //     alt: 'image-2'
-        //   },
-        //   {
-        //     src : 'https://cnet1.cbsistatic.com/img/aJE3TOIiSEqz_oFN80Y_1dQ0ucg=/1092x0/2019/08/16/99be5eac-2d91-4011-8c5e-89f328d88e67/geforce-rtx-2080-ti-web-tech-shot-630-u2x.png',
-        //     alt: 'image-3'
-        //   },
-        //   {
-        //     src : 'https://cnet1.cbsistatic.com/img/aJE3TOIiSEqz_oFN80Y_1dQ0ucg=/1092x0/2019/08/16/99be5eac-2d91-4011-8c5e-89f328d88e67/geforce-rtx-2080-ti-web-tech-shot-630-u2x.png',
-        //     alt: 'image-3'
-        //   },
-        //   {
-        //     src : 'https://cnet1.cbsistatic.com/img/aJE3TOIiSEqz_oFN80Y_1dQ0ucg=/1092x0/2019/08/16/99be5eac-2d91-4011-8c5e-89f328d88e67/geforce-rtx-2080-ti-web-tech-shot-630-u2x.png',
-        //     alt: 'image-3'
-        //   },
-        //   {
-        //     src : 'https://cnet1.cbsistatic.com/img/aJE3TOIiSEqz_oFN80Y_1dQ0ucg=/1092x0/2019/08/16/99be5eac-2d91-4011-8c5e-89f328d88e67/geforce-rtx-2080-ti-web-tech-shot-630-u2x.png',
-        //     alt: 'image-3'
-        //   },
+        image: [
+          {
+            src : 'https://cnet1.cbsistatic.com/img/aJE3TOIiSEqz_oFN80Y_1dQ0ucg=/1092x0/2019/08/16/99be5eac-2d91-4011-8c5e-89f328d88e67/geforce-rtx-2080-ti-web-tech-shot-630-u2x.png',
+            alt: 'image-1'
+          },
+          {
+            src : 'https://cnet1.cbsistatic.com/img/aJE3TOIiSEqz_oFN80Y_1dQ0ucg=/1092x0/2019/08/16/99be5eac-2d91-4011-8c5e-89f328d88e67/geforce-rtx-2080-ti-web-tech-shot-630-u2x.png',
+            alt: 'image-2'
+          },
+          {
+            src : 'https://cnet1.cbsistatic.com/img/aJE3TOIiSEqz_oFN80Y_1dQ0ucg=/1092x0/2019/08/16/99be5eac-2d91-4011-8c5e-89f328d88e67/geforce-rtx-2080-ti-web-tech-shot-630-u2x.png',
+            alt: 'image-3'
+          },
+          {
+            src : 'https://cnet1.cbsistatic.com/img/aJE3TOIiSEqz_oFN80Y_1dQ0ucg=/1092x0/2019/08/16/99be5eac-2d91-4011-8c5e-89f328d88e67/geforce-rtx-2080-ti-web-tech-shot-630-u2x.png',
+            alt: 'image-3'
+          },
+          {
+            src : 'https://cnet1.cbsistatic.com/img/aJE3TOIiSEqz_oFN80Y_1dQ0ucg=/1092x0/2019/08/16/99be5eac-2d91-4011-8c5e-89f328d88e67/geforce-rtx-2080-ti-web-tech-shot-630-u2x.png',
+            alt: 'image-3'
+          },
+          {
+            src : 'https://cnet1.cbsistatic.com/img/aJE3TOIiSEqz_oFN80Y_1dQ0ucg=/1092x0/2019/08/16/99be5eac-2d91-4011-8c5e-89f328d88e67/geforce-rtx-2080-ti-web-tech-shot-630-u2x.png',
+            alt: 'image-3'
+          },
           
-        // ],
-        // type: 'graphics card',
-        // name: 'rtx 2080',
-        // price: '2000',
-        // currency: '$',
-        // maxq : 15,
-        // description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sit saepe, asperiores molliti',
-        // keyFeatures:[
-        //   {
-        //     key: 'gpu architecture',
-        //     value: 'TURING'
-        //   },
-        //   {
-        //     key: 'boost clock',
-        //     value: '1800 MHz (OC)'
-        //   },
-        //   {
-        //     key: 'frame buffer',
-        //     value: '8 GB GDDR6'
-        //   },
-        //   {
-        //     key: 'memory speed',
-        //     value: '14 GBPS'
-        //   },
-        // ]
+        ],
+        type: 'graphics card',
+        name: 'rtx 2080',
+        price: '2000',
+        currency: '$',
+        maxq : 15,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sit saepe, asperiores molliti',
+        keyFeatures:[
+          {
+            key: 'gpu architecture',
+            value: 'TURING'
+          },
+          {
+            key: 'boost clock',
+            value: '1800 MHz (OC)'
+          },
+          {
+            key: 'frame buffer',
+            value: '8 GB GDDR6'
+          },
+          {
+            key: 'memory speed',
+            value: '14 GBPS'
+          },
+        ]
       }
     };
   },
@@ -168,6 +168,12 @@ export default {
       }
       else if(this.quantity < this.qtymin){
         this.quantity = this.qtymin;
+      }
+    },
+    wait: function(){
+      if(this.wait){
+        this.animation()
+        this.qtymax = this.product.maxq < 20 ? this.product.maxq : 20
       }
     },
   },
@@ -199,8 +205,6 @@ export default {
 
   mounted(){
 
-      this.animation()
-      this.qtymax = this.product.maxq < 20 ? this.product.maxq : 20
 
     
     
