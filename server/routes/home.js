5af04c523d6fbb5bddf5bec9be7c3b9f23cffbf8
AJSTINGER98 <amann.jha@gmail.com>
2020-08-11 18:13:30 +0530

    add news route

--- server/routes/home.js
+++ server/routes/home.js
@@ -7,5 +7,16 @@ router.get('/', (req,res) =>{
     });
 });
 
+router.get('/news',(req,res) =>{
+    var query = News.find({}).sort({_id:-1}).limit(7);
+    query.exec((err,News)=>{
+        if(err || !News){
+            console.log('No News to show');
+        } else{
+            res.json({News});
+        }
+    });
+});
+
 
 module.exports = router;
